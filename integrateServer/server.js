ba69bdb6563386451f0b0e679eb88f5856edd482
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-02 17:44:50 +0300

    db connecton and fetching

--- integrateServer/server.js
+++ integrateServer/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const sql = require('mssql');
 const { config } = require('./sqlconfig');
+const nodemailer = require('nodemailer')
 
 require('dotenv').config();
 
@@ -58,7 +59,7 @@ const users = [
 
 // home route
 app.get('/', (req, res) => {
-    res.send("Welcome G")
+    res.send({message:"Welcome G"})
 })
 
 app.get('/users', async (req, res) => {
@@ -140,6 +141,53 @@ app.delete('users:id', (req, res) => {
     let newUsers = users.filter(user => user.id !== Number(id))
     res.json(newUsers);
 })
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'standrewswomengroup@gmail.com',
+        pass: 'edinqmqllshvowss'
+        // user: 'korinjoroge63@gmail.com',
+        // pass: 'kyfbyyavzjfjlmii'
+    }
+});
+
+app.get('/sendmail', (req, res) => {
+    console.log("send mail route")
+    const mailOptions = {
+        from: 'standrewswomengroup@gmail.com',
+        to: `korinjoroge63@gmail.com`,
+        subject: 'Registration!',
+        text: `Hello,Kori 
+        Your Registration was successful,
+        welcome.
+        Thank you `
+    };
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     console.log("we sending")
+    //     if (error) {
+    //         res.send(error)
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
+
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Server is ready to take our messages");
+        }
+    });
+});
+
+
+
+
+
+
 
 const port = process.env.PORT || 5555;
 app.listen(port, () => { console.log(`Server listening too port ${port}`) })
